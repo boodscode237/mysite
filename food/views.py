b42605ce4ec8d524200dcd395ec1026e3c9bcc09
boodscode237 <boodscode@gmail.com>
2022-08-03 01:12:12 +0500

    Added navbbar

--- food/views.py
+++ food/views.py
@@ -6,14 +6,25 @@ from .models import Item
 # Create your views here.
 def index(request):
     item_list = Item.objects.all()
-    template = loader.get_template('food/index.html')
+    # template = loader.get_template('food/index.html')
     context = {
-
+        'item_list': item_list,
     }
-    return HttpResponse(template.render(context, request))
+    path_ = 'food/index.html'
+    return render(request, path_, context)
+    # return HttpResponse(template.render(context, request))
 
 def item(request):
     return HttpResponse('<h1>This is an item view</h1>')
+
+def detail(request, item_id):
+    item = Item.objects.get(pk=item_id)
+    context = {
+        'item': item,
+    }
+    path_ = 'food/detail.html'
+    return render(request, path_, context)
+
 
 
 
--- mysite/settings.py
+++ mysite/settings.py
@@ -121,7 +121,16 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/food/static/food/',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+
